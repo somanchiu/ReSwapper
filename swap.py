d62dde56af0f1226a9ba823b559975df9d0e1330
blend-er <arialatom20@gmail.com>
2024-12-28 11:23:24 +0800

    add no-align option to swap.py for 1:1 inference

--- swap.py
+++ swap.py
@@ -7,19 +7,20 @@ import Image
 from insightface.app import FaceAnalysis
 import face_align
 
+from StyleTransferModel_128 import StyleTransferModel
+
 faceAnalysis = FaceAnalysis(name='buffalo_l')
 faceAnalysis.prepare(ctx_id=0, det_size=(512, 512))
 
-from StyleTransferModel_128 import StyleTransferModel
-
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Process command line arguments')
-    
+
     parser.add_argument('--target', required=True, help='Target path')
     parser.add_argument('--source', required=True, help='Source path')
     parser.add_argument('--outputPath', required=True, help='Output path')
     parser.add_argument('--modelPath', required=True, help='Model path')
     parser.add_argument('--no-paste-back', action='store_true', help='Disable pasting back the swapped face onto the original image')
+    parser.add_argument('--no-align', action='store_true', help='Runs swapper model directly without face alignment and without paste back.')
     parser.add_argument('--resolution', type=int, default=128, help='Resolution')
 
     return parser.parse_args()
@@ -42,20 +43,23 @@ def swap_face(model, target_face, source_face_latent):
 
     with torch.no_grad():
         swapped_tensor = model(target_tensor, source_tensor)
-    
+
     swapped_face = Image.postprocess_face(swapped_tensor)
-    
+
     return swapped_face, swapped_tensor
 
-def create_target(target_image, resolution):
+def create_target(target_image, resolution, align=True):
     if isinstance(target_image, str):
         target_image = cv2.imread(target_image)
 
-    target_face = faceAnalysis.get(target_image)[0]
-    aligned_target_face, M = face_align.norm_crop2(target_image, target_face.kps, resolution)
-    target_face_blob = Image.getBlob(aligned_target_face, (resolution, resolution))
+    if align:
+        target_face = faceAnalysis.get(target_image)[0]
+        aligned_target_face, M = face_align.norm_crop2(target_image, target_face.kps, resolution)
+        target_face_blob = Image.getBlob(aligned_target_face, (resolution, resolution))
+        return target_face_blob, M
 
-    return target_face_blob, M
+    target_face_blob = Image.getBlob(target_image, (resolution, resolution))
+    return target_face_blob, None
 
 def create_source(source_img_path):
     source_image = cv2.imread(source_img_path)
@@ -68,7 +72,7 @@ def create_source(source_img_path):
 
 def main():
     args = parse_arguments()
-    
+
     # Access the arguments
     target_image_path = args.target
     source = args.source
@@ -78,11 +82,11 @@ def main():
     model = load_model(model_path)
 
     target_img = cv2.imread(target_image_path)
-    target_face_blob, M = create_target(target_img, args.resolution)
+    target_face_blob, M = create_target(target_img, args.resolution, not args.no_align)
     source_latent = create_source(source)
     swapped_face, _ = swap_face(model, target_face_blob, source_latent)
 
-    if not args.no_paste_back:
+    if not args.no_paste_back and not args.no_align:
         swapped_face = Image.blend_swapped_image(swapped_face, target_img, M)
 
     output_folder = os.path.dirname(output_path)
